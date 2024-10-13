f6ff8554f89d03244109ebf980d184af110314d4
MaulikVerma1 <82905199+MaulikVerma1@users.noreply.github.com>
2024-10-13 12:02:38 -0400

    Update AuthProvider.tsx

--- app/AuthProvider.tsx
+++ app/AuthProvider.tsx
@@ -1,32 +1,20 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useFirebase } from '../hooks/useFirebase';
-import { User } from 'firebase/auth';
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
   const { auth } = useFirebase();
-  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     if (auth) {
-      const unsubscribe = auth.onAuthStateChanged((authUser) => {
-        if (authUser) {
-          setUser(authUser);
-        } else {
-          setUser(null);
-        }
+      const unsubscribe = auth.onAuthStateChanged(() => {
+        // You can add logic here if needed
       });
 
       return () => unsubscribe();
     }
   }, [auth]);
 
-  return (
-    <div>
-      {/* Your layout components */}
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
-
